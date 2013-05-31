0c0f81aedbbdbf59fe6b8b7c80f4f57fcd913626
arvicco <arvicco@gmail.com>
2013-05-30 19:03:33 -0700

    Miner node improved

--- lib/avalon/miner.rb
+++ lib/avalon/miner.rb
@@ -9,18 +9,36 @@ module Avalon
   # Miner is a node encapsulating a single Avalon unit
   class Miner < Node
 
+    # type = :absolute_time | :absolute_date | :relative_time
+    def self.my_time t, type=:absolute_time
+      time = Time.at(t.to_f)
+      case type
+      when :absolute_date
+        time.getlocal.strftime("%Y-%m-%d %H:%M:%S")
+      when :absolute_time
+        time.getlocal.strftime("%H:%M:%S")
+      when :relative_time
+        time.utc.strftime("#{(time.day-1)*24+time.hour}:%M:%S")
+      end
+    end
+
+
     # Field formats: name => [width, pattern, type/conversion]
     FIELDS = { :ping => [8, /./, nil],  # not in miner status string...
-               :mhs => [10, /MHS av=([\d\.]*)/, :f],
-               :uptime => [6, /Elapsed=([\d\.]*)/, ->(x){ (x.to_i/60.0/60.0).round(2)}],
-               :utility => [7, /,Utility=([\d\.]*)/, :f],
-               :getworks => [8, /Getworks=([\d\.]*)/, :i],
-               :accepted => [8, /,Accepted=([\d\.]*)/, :i],
-               :rejected => [8, /Rejected=([\d\.]*)/, :i],
-               :stale => [6, /Stale=([\d\.]*)/, :i],
-               :errors => [6, /Hardware Errors=([\d\.]*)/, :i],
-               :blocks => [6, /Network Blocks=([\d\.]*)/, :i],
-               :found => [2, /Found Blocks=([\d\.]*)/, :i],
+               :mhs => [6, /(?<=MHS av=)[\d\.]*/, :i],
+               # :uptime => [6, /(?<=Elapsed=)[\d\.]*/, ->(x){ (x.to_i/60.0/60.0).round(2)}],
+               :uptime => [8, /(?<=Elapsed=)[\d\.]*/, ->(x){ my_time(x, :relative_time)}],
+               :last => [8, /(?<=Last Share Time=)[\d\.]*/,
+                         ->(x){ my_time(Time.now.getgm-x.to_i, :relative_time)}],
+                         # ->(x){ my_time(Time.now-Time.at(x.to_i), :relative_time)}],
+               :utility => [7, /(?<=,Utility=)[\d\.]*/, :f],
+               :getworks => [8, /(?<=Getworks=)[\d\.]*/, :i],
+               :accepted => [8, /(?<=,Accepted=)[\d\.]*/, :i],
+               :rejected => [8, /(?<=Rejected=)[\d\.]*/, :i],
+               :stale => [6, /(?<=Stale=)[\d\.]*/, :i],
+               :errors => [6, /(?<=Hardware Errors=)[\d\.]*/, :i],
+               :blocks => [6, /(?<=Network Blocks=)[\d\.]*/, :i],
+               :found => [2, /(?<=Found Blocks=)[\d\.]*/, :i],
                }
 
     def self.print_headers
@@ -32,6 +50,7 @@ module Avalon
       @ip = ip
       @num = ip.split('.').last.to_i
       @min_speed = min_speed * 1000 # Gh/s to Mh/s
+      @blanks = 0
       super()
     end
 
@@ -42,7 +61,7 @@ module Avalon
       else
         # Convert the status string into usable data pairs
         pairs = FIELDS.map do |name, (_, pattern, type)|
-          value_str = status.match(pattern)[1]
+          value_str = status[pattern] #[1]
           if type.is_a?(Symbol)
             [name, value_str.send("to_#{type}")]
           elsif type.respond_to?(:call)
@@ -55,24 +74,36 @@ module Avalon
       end
     end
 
+    def get api_call
+      self[:ping] ? `bash -ic "echo -n '#{api_call}' | nc #{@ip} 4028"` : ""
+    end
+
     def poll verbose=true
       self[:ping] = ping @ip
 
-      status = self[:ping] ? `bash -ic "echo -n 'summary' | nc #{@ip} 4028"` : ""
+      status = get('summary') + get('pools')
 
       extract_data_from status
 
       puts "#{self}" if verbose
     end
 
+    def upminutes
+      Time.utc(1970, 01, 01, *self[:uptime].split(/:/).map(&:to_i)).to_i/60.0
+    end
+
     # Check for any exceptional situations in stats, sound alarm if any
     def report
       if data.empty?
-        alarm "Miner #{@num} did not respond to status query"
-      elsif self[:mhs] < @min_speed and self[:uptime] > 0.1
-        alarm "Miner #{@num} performance is #{self[:mhs]}, should be #{@min_speed}"
-      elsif self[:uptime] < 0.05
-        alarm "Miner #{@num} restarted", "Frog.aiff"
+        @blanks += 1
+        alarm "Miner #{@num} did not respond to status query" if @blanks > 1
+      else
+        @blanks = 0
+        if self[:mhs] < @min_speed*0.95 and upminutes > 5
+          alarm "Miner #{@num} performance is #{self[:mhs]}, should be #{@min_speed}"
+        elsif upminutes < 2
+          alarm "Miner #{@num} restarted", "Frog.aiff"
+        end
       end
     end
 
