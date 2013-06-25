0a36b687ccb8369a313e188709c008f833c3c73a
arvicco <arvicco@gmail.com>
2013-06-24 18:42:12 -0700

    Refactor sound config

--- lib/avalon/config.rb
+++ lib/avalon/config.rb
@@ -3,6 +3,14 @@ module Avalon
   class Config
     extend Utils # Helper methods
 
+    DEFAULT_SOUNDS =       {
+      failure: 'Glass.aiff',
+      restart: 'Frog.aiff',
+      temp_high: 'Ping.aiff',
+      block_found: ['Dog.aiff', 'Purr.aiff', 'Dog.aiff'],
+      block_updated: ['Purr.aiff', 'Purr.aiff', 'Purr.aiff']
+    }
+
     def self.load env
       config_file = find_file( '../../../config/monitor.yml', '~/.avalon/monitor.yml')
 
@@ -13,13 +21,7 @@ module Avalon
       @config[:block_file] =  find_file( '../../../config/blocks.yml', '~/.avalon/blocks.yml') ||
         File.expand_path('~/.avalon/blocks.yml')
 
-      @config[:alert_sounds] ||= {
-        failure: 'Glass.aiff',
-        restart: 'Frog.aiff',
-        temp_high: 'Ping.aiff',
-        block_found: ['Dog.aiff', 'Purr.aiff', 'Dog.aiff'],
-        block_updated: ['Purr.aiff', 'Purr.aiff', 'Purr.aiff']
-      }
+      @config[:alert_sounds] = DEFAULT_SOUNDS.merge(@config[:alert_sounds] || {})
     end
 
     def self.[] key
